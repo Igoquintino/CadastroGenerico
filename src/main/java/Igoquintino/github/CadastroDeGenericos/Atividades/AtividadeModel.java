fc0b94b266ba7dd8f53983d5a5d78d850e634bd6
Igo Quintino <igocastro.15@gmail.com>
2026-04-17 15:20:38 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/Igoquintino/github/CadastroDeGenericos/Atividades/AtividadeModel.java
+++ src/main/java/Igoquintino/github/CadastroDeGenericos/Atividades/AtividadeModel.java
@@ -3,11 +3,17 @@ package Igoquintino.github.CadastroDeGenericos.Atividades;
 
 import Igoquintino.github.CadastroDeGenericos.Genericos.GenericoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_atividade")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AtividadeModel {
 
     @Id
@@ -21,55 +27,4 @@ public class AtividadeModel {
     // @OneToMany Uma atividade pode ter varios genericos
     @OneToMany(mappedBy = "atividadeModel")
     private List<GenericoModel> genericoModelList;
-
-
-    public AtividadeModel() {
-    }
-
-    public AtividadeModel(Long id, String nomeAtividade, String nivel) {
-        this.id = id;
-        this.nomeAtividade = nomeAtividade;
-        this.nivel = nivel;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAtividade() {
-        return nomeAtividade;
-    }
-
-    public void setAtividade(String nomeAtividade) {
-        this.nomeAtividade = nomeAtividade;
-    }
-
-    public String getNivel() {
-        return nivel;
-    }
-
-    public void setNivel(String nivel) {
-        this.nivel = nivel;
-    }
-
-    public String getNomeAtividade() {
-        return nomeAtividade;
-    }
-
-    public void setNomeAtividade(String nomeAtividade) {
-        this.nomeAtividade = nomeAtividade;
-    }
-
-    @Override
-    public String toString() {
-        return "AtividadeModel{" +
-                "id=" + id +
-                ", nomeAtividade='" + nomeAtividade + '\'' +
-                ", nivel='" + nivel +
-                '}';
-    }
 }
--- src/main/java/Igoquintino/github/CadastroDeGenericos/Genericos/GenericoModel.java
+++ src/main/java/Igoquintino/github/CadastroDeGenericos/Genericos/GenericoModel.java
@@ -2,12 +2,19 @@ package Igoquintino.github.CadastroDeGenericos.Genericos;
 
 import Igoquintino.github.CadastroDeGenericos.Atividades.AtividadeModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 // Entity ele tranforma uma classe em uma entidade do BD
 @Entity
 @Table(name = "tb_cadastro")
+@Data // Getters e Setters
+@NoArgsConstructor // construtor sem argumento
+@AllArgsConstructor // construtor com todos argumentos
 public class GenericoModel {
 
     @Id // mostra que o proximo, no caso Log id é um id da entidade que vai ser tabela
@@ -24,47 +31,4 @@ public class GenericoModel {
     @ManyToOne
     @JoinColumn(name = "atividades_id") // Foreing key ou chave estrageira
     private AtividadeModel atividadeModel;
-
-    public GenericoModel() {
-    }
-
-    public GenericoModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    @Override
-    public String toString() {
-        return "GenericoModel{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", email='" + email + '\'' +
-                ", idade=" + idade +
-                '}';
-    }
 }
